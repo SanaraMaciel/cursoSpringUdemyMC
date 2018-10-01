de832c56c923b7c315bed4d888e71cc1b150d295
Sanara felício <Sanarafm27@gmail.com>
2018-10-01 11:48:33 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/sanarafelicio/cursomc/CursomcApplication.java
+++ src/main/java/com/sanarafelicio/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sanarafelicio.cursomc.domain.Categoria;
+import com.sanarafelicio.cursomc.domain.Produto;
 import com.sanarafelicio.cursomc.repositories.CategoriaRepository;
+import com.sanarafelicio.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,8 +30,27 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		
+		//instanciando 3 produtos
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		
+		//Adicionando os produtos na lista de categoria
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		//Adicionando as categorias na lista de produtos
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		//salvando e criando uma lista automática
 		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 	}
 	
--- src/main/java/com/sanarafelicio/cursomc/domain/Categoria.java
+++ src/main/java/com/sanarafelicio/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.sanarafelicio.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -16,6 +19,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	//constructor empty
 	public Categoria() {
@@ -43,6 +49,14 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -68,8 +82,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-		
-	
 	
 }
