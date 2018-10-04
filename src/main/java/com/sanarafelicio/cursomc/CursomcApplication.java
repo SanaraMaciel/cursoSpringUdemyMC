5a4ff363b3e89fea51f55c9bf222e81cdf354b26
Sanara felício <Sanarafm27@gmail.com>
2018-10-04 11:19:33 -0300

    Paginação com parâmetros adicionais na requisição

--- src/main/java/com/sanarafelicio/cursomc/CursomcApplication.java
+++ src/main/java/com/sanarafelicio/cursomc/CursomcApplication.java
@@ -70,8 +70,14 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//instanciando categorias	
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Cama mesa e Banho");
+		Categoria cat4 = new Categoria(null,"Eletrônicos");
+		Categoria cat5 = new Categoria(null,"Jardinagem");
+		Categoria cat6 = new Categoria(null,"Decoração");
+		Categoria cat7 = new Categoria(null,"Perfumaria");
 		
 		
 		//instanciando 3 produtos
@@ -91,7 +97,7 @@ public class CursomcApplication implements CommandLineRunner{
 					
 		
 		//salvando e criando uma lista automática
-		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		categoriaRepository.save(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 		//Instanciação um estado
