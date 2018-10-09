1f201c1b29d50cbe830da3bab6a34597b0e531a7
Sanara felício <Sanarafm27@gmail.com>
2018-10-09 11:57:06 -0300

    Validação customizada email não permitido na atualização de cliente

--- src/main/java/com/sanarafelicio/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/sanarafelicio/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.sanarafelicio.cursomc.domain.Cliente;
+import com.sanarafelicio.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
