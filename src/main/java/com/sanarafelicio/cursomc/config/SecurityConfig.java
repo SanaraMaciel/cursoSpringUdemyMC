7f27faecea97df4f16ab1f29841fe26aff0a3697
Sanara felício <Sanarafm27@gmail.com>
2018-10-16 10:40:04 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/sanarafelicio/cursomc/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.sanarafelicio.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+	@Autowired
+	private Environment env;
+	
+
+	//vetor para definir quais caminhos q por padrão vão estar liberados da autenticação  
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
+	
+	
+	//vetor para definir quais caminhos somente leitura  
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	}; 
+	
+	
+	
+	//configuração de permissão de url 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+		//anotação para permitir somente o método get para essas listas
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	//configuração de Cors
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+	
+	
+}
