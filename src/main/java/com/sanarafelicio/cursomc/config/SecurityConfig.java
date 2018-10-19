99b80a0e6e0625488cd94ef0075de52fc8d16879
Sanara felício <Sanarafm27@gmail.com>
2018-10-19 09:23:55 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/sanarafelicio/cursomc/config/SecurityConfig.java
+++ src/main/java/com/sanarafelicio/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.sanarafelicio.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	
@@ -46,11 +48,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	//vetor para definir quais caminhos somente leitura  
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
-			"/clientes/**"
+			"/categorias/**"
+			
 	}; 
 	
-	
+	//vetor para definir quais caminhos de gravação como cadastro  
+		private static final String[] PUBLIC_MATCHERS_POST = {
+				"/clientes/**"
+		};
 	
 	//configuração de permissão de url 
 	@Override
@@ -62,6 +67,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+		//anotação para permitir requisições de POST
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		//anotação para permitir somente o método get para essas listas
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
